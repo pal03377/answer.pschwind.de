2eed9236b19c31f8c222560abff30c8f054a1ba1
Paul Schwind <paul@pschwind.de>
2022-01-15 23:29:10 +0100

    add Pls answer me :-( to main route again

--- main/src/routes/index.svelte
+++ main/src/routes/index.svelte
@@ -1,6 +1,1 @@
-<script>
-    console.log("Test");
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+Pls answer me :-(
